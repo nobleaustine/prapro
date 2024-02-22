d6a55b39db7360c8399fdd737205169b9ad82199
nobleaustine <nobleaustine2001@gmail.com>
2024-02-22 18:46:14 +0530

    task 12 completed used argparse and also created test and yml file for github actions

--- task_12/task_12.py
+++ task_12/task_12.py
@@ -22,15 +22,15 @@ def arithematic():
                                      epilog="program to learn how to pass arguments through command line")
 
     # add arguments for adding
-    parser.add_argument('arg1',metavar="a", type=int, help='first number')
-    parser.add_argument('arg2',metavar="b", type=int, help='second number')
+    parser.add_argument('arg1',metavar="a", type=float, help='first number')
+    parser.add_argument('arg2',metavar="b", type=float, help='second number')
     parser.add_argument("-m","--multiply",dest="operation",default=add,action="store_const",const=multiply,help="specify the operation o")
     args = parser.parse_args()
-
-    if args.operation is add :
-         print(f"{args.arg1} + {args.arg2} = {args.arg1 + args.arg2}")
-    else:
-         print(f"{args.arg1} x {args.arg2} = {args.arg1 * args.arg2}") 
+    
+    return args.arg1,args.arg2,args.operation(args.arg1,args.arg2)
+   
     
 if __name__ == "__main__":
-    arithematic()
+
+    a,b,c = arithematic()
+    print(f"{a} + {b} = {c}") if a + b == c else print(f"{a} x {b} = {c}")
