186e4c3a877d543cd0292f1ee93bb1f53ca3eca9
nobleaustine <nobleaustine2001@gmail.com>
2024-02-22 18:49:39 +0530

    formatted the code using black

--- task_12/task_12.py
+++ task_12/task_12.py
@@ -9,28 +9,41 @@
 
 import argparse
 
-def multiply(a,b):
-        return a * b
-    
-def add(a,b):
+
+def multiply(a, b):
+    return a * b
+
+
+def add(a, b):
     return a + b
+
 
 def arithematic():
     # creating parser object
-    parser = argparse.ArgumentParser(prog="arithematic",
-                                     description="pass intergers for addition and multiplication through command line",
-                                     epilog="program to learn how to pass arguments through command line")
+    parser = argparse.ArgumentParser(
+        prog="arithematic",
+        description="pass intergers for addition and multiplication through command line",
+        epilog="program to learn how to pass arguments through command line",
+    )
 
     # add arguments for adding
-    parser.add_argument('arg1',metavar="a", type=float, help='first number')
-    parser.add_argument('arg2',metavar="b", type=float, help='second number')
-    parser.add_argument("-m","--multiply",dest="operation",default=add,action="store_const",const=multiply,help="specify the operation o")
+    parser.add_argument("arg1", metavar="a", type=float, help="first number")
+    parser.add_argument("arg2", metavar="b", type=float, help="second number")
+    parser.add_argument(
+        "-m",
+        "--multiply",
+        dest="operation",
+        default=add,
+        action="store_const",
+        const=multiply,
+        help="specify the operation o",
+    )
     args = parser.parse_args()
-    
-    return args.arg1,args.arg2,args.operation(args.arg1,args.arg2)
-   
-    
+
+    return args.arg1, args.arg2, args.operation(args.arg1, args.arg2)
+
+
 if __name__ == "__main__":
 
-    a,b,c = arithematic()
+    a, b, c = arithematic()
     print(f"{a} + {b} = {c}") if a + b == c else print(f"{a} x {b} = {c}")
--- task_12/tests/test_arithematic.py
+++ task_12/tests/test_arithematic.py
@@ -1,23 +1,26 @@
-
 import subprocess
 import pytest
 
-l1 = [[2,3,5],[-1,3,-2],[3.2,-1.2,2]]
+l1 = [[2, 3, 5], [-1, 3, -2], [3.2, -1.2, 2]]
 
-@pytest.mark.parametrize("n",l1)
+
+@pytest.mark.parametrize("n", l1)
 def test_add(n):
     a = str(float(n[0]))
     b = str(float(n[1]))
     c = str(float(n[0]) + float(n[1]))
-    result = subprocess.run(['python', 'task_12.py', a, b], stdout=subprocess.PIPE)
+    result = subprocess.run(["python", "task_12.py", a, b], stdout=subprocess.PIPE)
     output = result.stdout.decode().strip()
     assert output == f"{a} + {b} = {c}"
 
-@pytest.mark.parametrize("n",l1)
+
+@pytest.mark.parametrize("n", l1)
 def test_add(n):
     a = str(float(n[0]))
     b = str(float(n[1]))
     c = str(float(n[0]) * float(n[1]))
-    result = subprocess.run(['python', 'task_12.py', a, b,'-m'], stdout=subprocess.PIPE)
+    result = subprocess.run(
+        ["python", "task_12.py", a, b, "-m"], stdout=subprocess.PIPE
+    )
     output = result.stdout.decode().strip()
     assert output == f"{a} x {b} = {c}"
